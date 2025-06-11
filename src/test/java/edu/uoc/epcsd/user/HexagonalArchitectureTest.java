e7c64a17ea7ff5409e2d1044f607c76c55c3f485
Mikel Goyeneche <info@mikelweb.net>
2025-06-11 08:20:55 +0200

    HexagonalArchitectureTest

---
+++ src/test/java/edu/uoc/epcsd/user/HexagonalArchitectureTest.java
@@ -0,0 +1,28 @@
+package edu.uoc.epcsd.user;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.Architectures.onionArchitecture;
+import org.springframework.stereotype.Service;
+
+@AnalyzeClasses(
+    packages = "edu.uoc.epcsd.user",
+    importOptions = {
+        ImportOption.DoNotIncludeTests.class,
+        ImportOption.DoNotIncludeJars.class
+    }
+)
+
+public class HexagonalArchitectureTest {
+
+    @ArchTest
+    static final ArchRule domain_service_with_spring_annotation = classes()
+        .that().resideInAPackage("..domain.service..")
+        .and().areAnnotatedWith(Service.class)
+        .should().haveSimpleNameEndingWith("ServiceImpl");
+
+
+
+
